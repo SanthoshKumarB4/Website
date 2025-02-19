d6c578ed71abdf46cb1e9878f0cca88f0649c93b
santhosh <santhosh.xyloite.2024@gmail.com>
2025-02-19 11:29:04 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Child from "./child";
 
-function App() {
+function Parent() {
+  const [childData, setDataChild] = useState('');
+
+
+  function handleDataChild(data) {
+    setDataChild(data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Data from Child: {childData}</h1  >
+      <Child sendDataToParent={handleDataChild} />
     </div>
   );
 }
 
-export default App;
+export default Parent;
